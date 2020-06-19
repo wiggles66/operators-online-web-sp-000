6538f6dc43e3256c748d98d3412934c5626b48af
Michael Waide <mvwaide@gmail.com>
2020-06-19 19:15:36 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,8 @@
-def unsafe?(speed)
-
+def unsafe? speed
+  speed < 40 || speed > 60
 end
 
 
-
-def not_safe?(speed)
-	
+	def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
 end
-	
-
-
